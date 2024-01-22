ebc6d53dc379b425cb52813f69638086e6e4c37d
santhosh250101 <santhosh250101@gmail.com>
2024-01-22 10:24:01 +0000

    styled using tailwind css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
-  return (
-    <div>Contact us</div>
-  )
+  return <div>Contact us</div>;
 }
 
-export default Contact
+export default Contact;
