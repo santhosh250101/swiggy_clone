fa16f1eefcd895b52c950289f48b3649b62c7de0
Papisetty Santhosh <santhosh250101@gmail.com>
2024-09-09 18:44:21 +0530

    implemented offline online hook

---
+++ src/utils/useOnlineStatus.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+  useEffect(() => {
+    window.addEventListener("offline", function () {
+      setOnlineStatus(false);
+    });
+    window.addEventListener("online", function () {
+      setOnlineStatus(true);
+    });
+  }, []);
+
+  return onlineStatus;
+};
+
+export default useOnlineStatus;
